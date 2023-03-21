2f2345a0b93eecb493fa7e252434e37384a2ae89
Pedro Araujo <pedro@syncnow.net>
2023-03-21 16:12:31 -0300

    Add files via upload

--- src/traits.h
+++ src/traits.h
@@ -18,7 +18,7 @@ struct Traits {
 
 template<> struct Traits<CPU>
 {
-    //Adicionar aqui a declaração do tamanho da Pilha
+    //Tamanho da pilha, valor atribuido usando como base o exemplo na descrição do trabalho  
     static const unsigned int STACK_SIZE = 16384;
 };
 
