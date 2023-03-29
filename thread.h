41ca023d74b79446d41fdd46484e70f375fdddaf
AlexisMS <alexisora@gmail.com>
2023-03-29 17:47:37 -0300

    desenvolvimento inicial

--- thread.h
+++ thread.h
@@ -20,7 +20,12 @@ public:
      * PS: devido ao template, este método deve ser implementado neste mesmo arquivo .h
      */ 
     template<typename ... Tn>
-    Thread(void (* entry)(Tn ...), Tn ... an);
+    Thread(void (* entry)(Tn ...), Tn ... an){
+        this->_context = new Context(void (* entry)(Tn ...), Tn ... an);
+        pthread_t thread = pthread_create(&_running, NULL, void (* entry)(Tn ...), Tn ... an);
+        this->_running = &thread;
+        pthread_join(_running, NULL);
+    }
 
     /*
      * Retorna a Thread que está em execução.
@@ -33,19 +38,27 @@ public:
      * Deve encapsular a chamada para a troca de contexto realizada pela class CPU.
      * Valor de retorno é negativo se houve erro, ou zero.
      */ 
-    static int switch_context(Thread * prev, Thread * next);
+    static int switch_context(Thread * prev, Thread * next){
+        this->_running = next;
+        CPU::switch_context(prev->_context, next->_context);
+    }
 
     /*
      * Termina a thread.
      * exit_code é o código de término devolvido pela tarefa (ignorar agora, vai ser usado mais tarde).
      * Quando a thread encerra, o controle deve retornar à main. 
      */  
-    void thread_exit (int exit_code);
+    void thread_exit (int exit_code){
+        this->_exit_code = exit_code;
+        pthread_exit(NULL);
+    }
 
     /*
      * Retorna o ID da thread.
      */ 
-    int id();
+    int id(){
+        return (int)gettid();
+    }
 
     /*
      * Qualquer outro método que você achar necessário para a solução.
@@ -55,6 +68,7 @@ private:
     int _id;
     Context * volatile _context;
     static Thread * _running;
+    int _exit_code;
 
     /*
      * Qualquer outro atributo que você achar necessário para a solução.
