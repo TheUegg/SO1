6afa60289fb2d11a85cf9b98ae7c206243222761
Gabriel Dutra <48719941+TheUegg@users.noreply.github.com>
2023-04-30 13:35:16 -0300

    T3
    (odeio a leitura de saída por causa do list.h)

--- thread.cc
+++ thread.cc
@@ -8,26 +8,160 @@ __BEGIN_API
 
 Thread * Thread::_running = NULL;
 unsigned int Thread::_thread_count = 0;
+Thread Thread::_main; 
+CPU::Context Thread::_main_context; 
+Thread Thread::_dispatcher;
+Thread::Ready_Queue Thread::_ready;
 
-int Thread::switch_context(Thread * prev, Thread * next){        
-        //Testa se o contesto é igual ao atual
-        if(prev == next) {
-            db<Thread>(ERR)<<"Thread::switch_context() - ERRO próximo contexto igual ao contexto atual\n";
-            return -1;
+    Thread::Ready_Queue::Element* Thread::link(){
+        return &_link;
+    }
+
+    void Thread::dispatcher(){
+        db<Thread>(TRC)<<"Thread::dispatcher()\n";
+        while(!_ready.empty() && _ready.head()->object() != &_dispatcher) {
+
+            Thread *next_thread = next(); //escolha uma próxima thread a ser executada
+            Thread *prev_thread = _running;
+
+            dequeue(next_thread, _ready);
+            Thread::_thread_count--;
+
+            enqueue(&_dispatcher, _ready);
+            _dispatcher.set_state(READY);
+            switch_context(prev_thread, next_thread);
+        }
+        _dispatcher.set_state(FINISHING);
+        dequeue(&_dispatcher, _ready);
+        switch_context(&_dispatcher, &_main);
+    }
+
+    /*
+     * Adiciona thread na fila de prontos.
+    */
+    void Thread::enqueue(Thread * thread,Thread::Ready_Queue &queue){
+	    db<Thread>(TRC)<<"Thread::enqueue(Thread * thread,Thread::System_Queue &queue)\n";
+
+        if(thread == 0)
+            return;
+
+        if(thread == &_dispatcher)
+            queue.insert_head(thread->link());
+        else{
+            thread->update_priority();
+            queue.insert(thread->link());
         }
 
-        //Troca a thread que está rodando para a proxima, trocando o contexto em seguida 
-        _running = next;
-        CPU::switch_context(prev->_context, next->_context);
+        db<Thread>(TRC)<<"Thread::insert_queue: Thread " << thread->id() << " inserida na fila ";
+        if(&queue == &_ready){
+            db<Thread>(TRC)<<" _ready\n";
+            thread->set_state(READY);
+        }
+    }
+
+    void Thread::dequeue(Thread * thread, Thread::Ready_Queue &queue){
+
+		db<Thread>(TRC)<<"Thread::dequeue(Thread * thread, Thread::System_Queue &queue)\n";
+		queue.remove(thread->link());
+
+		db<Thread>(TRC)<<"Thread::remove_queue: Thread " << thread->id() << " removida da fila ";
+		if(&queue == &_ready)
+			db<Thread>(TRC)<<" _ready\n";
+	}
+
+    void Thread::init(void (*main)(void *)){
+		db<Thread>(TRC) << "Thread::init(void (*main)(void *))\n";
+
+		new (&_main_context) Context();
+		db<Thread>(TRC) << "Thread::init: Main context criado\n";
+	
+        new (&_main) Thread(main, (void *) "main"); 
+		db<Thread>(TRC) << "Thread::init: Main thread criada\n";
+		
+		dequeue(&_main, _ready);
+		_running = &_main;
+
+        new (&_dispatcher) Thread((void (*) (void *)) &Thread::dispatcher, (void *) NULL);
+
+        db<Thread>(TRC) << "Thread::init: Dispatcher thread foi criada\n";	
+
+		CPU::switch_context(&_main_context, _main.context());
+	}
+
+    int Thread::switch_context(Thread * prev, Thread * next) 
+    {
+        db<Thread>(TRC)<<"Thread::switch_context()\n";
+        if (prev != next) {
+            _running = next;
+            next->set_state(RUNNING);
+
+            CPU::switch_context(prev->_context, next->_context);
+        }
         return 0;
-}
+    }
 
-void Thread::thread_exit(int exit_code){
-    db<Thread>(TRC)<<"Thread::thread_exit()\n";
-    //libera a thread e seu contexto
-    this->_running = NULL;
-    delete _context;
-}
+     void Thread::set_state(Thread::State state)
+    {
+        db<Thread>(TRC)<<"Thread::set_state(): Thread " << this->id() << " => ";
 
+        switch(state){
+            case 0:
+                db<Thread>(TRC)<<"RUNNING\n";
+                break;
+            case 1:
+                db<Thread>(TRC)<<"READY\n";
+                break;
+        }
+        _state = state;
+    }
+
+    void Thread::update_priority()
+    {
+		db<Thread>(TRC)<<"Thread::update_priority()\n";
+
+		int now = std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock::now().time_since_epoch()).count();
+		link()->rank(now);
+		db<Thread>(TRC)<<"Thread::update_priority: Thread " << id() << " prioridade atualizada para " << now << "\n";
+    }
+
+
+    void Thread::yield()
+    {
+		db<Thread>(TRC)<<"Thread::yield()\n";
+		Thread *next_thread = next();
+		if(next_thread != 0){
+			if(_running->state() == RUNNING && _running != &_main) //se ela não estiver finishing e não for a main
+				enqueue(_running,_ready); //insere no ready
+
+			dequeue(next_thread, _ready);
+			switch_context(_running, next_thread);
+		}
+	}
+
+    Thread::State Thread::state() 
+    {
+        return _state;
+    }
+
+    Thread * Thread::next(){
+		if(_ready.size() == 0){
+			return 0;
+		}
+		return _ready.head()->object();
+	}
+
+    void Thread::thread_exit(int exit_code) {
+        db<Thread>(TRC)<<"Thread::exit()\n";
+        set_state(FINISHING);
+        Thread::yield();
+    }
+
+    Thread::~Thread(){
+		db<Thread>(TRC) << "Thread~Thread()\n";
+
+        _ready.remove(this);
+
+		delete _context;
+	}
 
 __END_API
--- thread.h
+++ thread.h
@@ -4,6 +4,9 @@
 #include "cpu.h"
 #include "traits.h"
 #include "debug.h"
+#include "list.h"
+#include <ctime> 
+#include <chrono>
 
 __BEGIN_API
 
@@ -11,8 +14,22 @@ class Thread
 {
 protected:
     typedef CPU::Context Context;
-
 public:
+
+    typedef Ordered_List<Thread> Ready_Queue;
+
+    // Thread State
+    enum State {
+        RUNNING,
+        READY,
+        FINISHING
+    };
+
+    /*
+     * Construtor vazio. Necessário para inicialização, mas sem importância para a execução das Threads.
+     */ 
+    Thread() { }
+
     /*
      * Cria uma Thread passando um ponteiro para a função a ser executada
      * e os parâmetros passados para a função, que podem variar.
@@ -20,14 +37,7 @@ public:
      * PS: devido ao template, este método deve ser implementado neste mesmo arquivo .h
      */ 
     template<typename ... Tn>
-    Thread(void (* entry)(Tn ...), Tn ... an){
-        db<Thread>(TRC)<<"Thread()\n";
-
-        //Coloca o id da thread pelo contador, e inicia o contexto da thread
-        _id = _thread_count;
-        _thread_count++;
-        _context = new Context(entry, an...);
-    }
+    Thread(void (* entry)(Tn ...), Tn ... an);
 
     /*
      * Retorna a Thread que está em execução.
@@ -43,12 +53,11 @@ public:
     static int switch_context(Thread * prev, Thread * next);
 
     /*
-     * Termina a thread.    
+     * Termina a thread.
      * exit_code é o código de término devolvido pela tarefa (ignorar agora, vai ser usado mais tarde).
      * Quando a thread encerra, o controle deve retornar à main. 
      */  
-    void thread_exit (int exit_code);//{
-        
+    void thread_exit (int exit_code);
 
     /*
      * Retorna o ID da thread.
@@ -58,25 +67,88 @@ public:
         return Thread::_id;
     }
 
-    Context * volatile context(){
-        return _context;
-    }
-    
+    /*
+     * NOVO MÉTODO DESTE TRABALHO.
+     * Daspachante (disptacher) de threads. 
+     * Executa enquanto houverem threads do usuário.
+     * Chama o escalonador para definir a próxima tarefa a ser executada.
+     */
+    static void dispatcher(); 
+
+    /*
+     * NOVO MÉTODO DESTE TRABALHO.
+     * Realiza a inicialização da class Thread.
+     * Cria as Threads main e dispatcher.
+     */ 
+    static void init(void (*main)(void *));
+
+
+    /*
+     * Devolve o processador para a thread dispatcher que irá escolher outra thread pronta
+     * para ser executada.
+     */
+    static void yield(); 
+
+    /*
+     * Destrutor de uma thread. Realiza todo os procedimentos para manter a consistência da classe.
+     */ 
+    ~Thread();
+
     /*
      * Qualquer outro método que você achar necessário para a solução.
      */ 
 
+    Context * volatile context(){
+        return _context;
+    }
+
+    static void enqueue(Thread * thread, Ready_Queue &queue);
+
+    static void dequeue(Thread * thread, Ready_Queue &queue);
+
+    Ready_Queue::Element * link();
+
+    void set_state(State state);
+
+    State state();
+
+    static Thread * next();
+
+    void update_priority();
+
 private:
-    int _id;    
+    int _id;
     Context * volatile _context;
     static Thread * _running;
+    
+    static Thread _main; 
+    static CPU::Context _main_context;
+    static Thread _dispatcher;
+    static Ready_Queue _ready;
+    Ready_Queue::Element _link;
+    volatile State _state;
 
     /*
      * Qualquer outro atributo que você achar necessário para a solução.
      */ 
-
     static unsigned int _thread_count;
+
 };
+
+template<typename ... Tn>
+inline Thread::Thread(void (* entry)(Tn ...), Tn ... an){
+
+    db<Thread>(TRC)<<"Thread::Thread(void (* entry)(Tn ...), Tn ... an)\n";
+    _id = _thread_count;
+    _thread_count++;
+    _context = new Context(entry, an...);
+
+    new (&_link) Ready_Queue::Element(this,(std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock::now().time_since_epoch()).count()));
+    enqueue(this, _ready);
+
+    db<Thread>(INF)<<"Thread::Thread(void (* entry)(Tn ...), Tn ... an): Contador de threads: = " << _thread_count << "\n";
+    db<Thread>(TRC)<<"Thread::Thread(void (* entry)(Tn ...), Tn ... an): Thread " << _id << " criada\n";
+}
 
 __END_API
 
